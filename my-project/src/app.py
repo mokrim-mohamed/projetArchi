8501f9fa5094eafd36ad9ebdcb8736e5e15cc7cd
mokrim <mokrimmohamed2016@gmail.com>
2024-07-22 14:52:44 +0200

    commit 2

--- my-project/src/app.py
+++ my-project/src/app.py
@@ -1,30 +1,42 @@
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
 @app.route('/')
 def hello_world():
-    return 'Hello, G3!'
-@app.route('/sum', methods=['POST'])
-def sum():
+    return 'Hello, Mr. Mokrmñ!'
+
+@app.route('/sum', methods=['GET'])
+def sum_route():
     try:
         a = request.args.get('a', type=float)
         b = request.args.get('b', type=float)
+
         if a is None or b is None:
             return jsonify({"error": "Les paramètres 'a' et 'b' sont requis"}), 400
-        result = a + b
+
+        result = add(a, b)
         return jsonify({"a": a, "b": b, "sum": result})
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-@app.route('/subtract', methods=['POST'])
-def subtract():
+@app.route('/subtract', methods=['GET'])
+def subtract_route():
     try:
         a = request.args.get('a', type=float)
         b = request.args.get('b', type=float)
+
         if a is None or b is None:
             return jsonify({"error": "Les paramètres 'a' et 'b' sont requis"}), 400
-        result = a - b
+
+        result = subtract(a, b)
         return jsonify({"a": a, "b": b, "difference": result})
 
     except Exception as e:
--- my-project/tests/test_main.py
+++ my-project/tests/test_main.py
@@ -1,2 +1,24 @@
-def test_main():
-    assert True
+import unittest
+from app import app
+class BasicTests(unittest.TestCase):
+    def test_sum_valid(self):
+        response = self.app.get('/sum?a=5&b=3')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {"a": 5.0, "b": 3.0, "sum": 8.0})
+    # Test de la route /sum avec des paramètres manquants
+    def test_sum_missing_params(self):
+        response = self.app.get('/sum?a=5')
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('error', response.json)
+    # Test de la route /subtract avec des paramètres valides
+    def test_subtract_valid(self):
+        response = self.app.get('/subtract?a=5&b=3')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {"a": 5.0, "b": 3.0, "difference": 2.0})
+    # Test de la route /subtract avec des paramètres manquants
+    def test_subtract_missing_params(self):
+        response = self.app.get('/subtract?a=5')
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('error', response.json)
+if __name__ == "__main__":
+    unittest.main()
